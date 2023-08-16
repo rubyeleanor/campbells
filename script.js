a311a6ddadd8af3f4bd0d06945b49643021bb4fa
rubyhay <115424833+rubyeleanor@users.noreply.github.com>
2023-08-16 21:36:57 +0100

    Amended modal script

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function greet() {
-    console.log('hiiii');
-}
+const images = document.querySelectorAll(".thumbs");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector("#modal-image");
+const close = document.querySelector(".close");
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modalImg.src = image.src;
+        modal.style.display = "block";
+
+        close.addEventListener("click", () => {
+            modal.style.display = "none";
+        })
+    })
+})
+
